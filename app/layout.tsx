945f6cff5f9a292019efa56ab98b2f1e18279b0e
Jedidta Adoni Saputra <adoni.091203@gmail.com>
2023-11-13 00:47:00 +0700

    feat: create theme and connect to covex and clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,26 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ConvexClientProvider } from "@/components/providers/convex-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Create Next App",
+  title: "NotreCraft",
   description: "Mengkoneksikan lembar kerja dengan cepat",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/favicon-dark.ico",
+      },
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/favicon-dark.ico",
+      }
+    ]
+  }
 };
 
 export default function RootLayout({
@@ -16,7 +30,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ConvexClientProvider>
+          <ThemeProvider 
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+            storageKey="theme"
+          >
+            {children}
+          </ThemeProvider>
+        </ConvexClientProvider>
+      </body>
     </html>
   );
 }
